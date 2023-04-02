9e7bc9b7c835bf38ecf24feca9297a59b7c6da71
Razif <fatahillah.razif@gmail.com>
2023-04-02 19:33:54 +0700

    create function callSayHelloClientStream

---
+++ client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/fataraz/go-grpc/proto"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewGreetServiceClient(conn)
+	names := &pb.NameList{
+		Names: []string{"Razif", "Fatahillah", "Kahfi"},
+	}
+
+	//callSayHello(client)
+	//callSayHelloServerStream(client, names)
+	callSayHelloClientStream(client, names)
+	//callHelloBidirectionalStream(client, names)
+}
